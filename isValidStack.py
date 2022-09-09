9c8b82b73feb953c9ae5701a444c4f81672367e1
Shubh <5046138+whitehawk0011@users.noreply.github.com>
2022-09-09 19:53:24 +0530

    Create isValidStack.py

---
+++ isValidStack.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s :
+            if char in ['(', '{',  '[' ]:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                current_char = stack.pop()
+                if current_char == '(':
+                    if char != ")":
+                        return False
+                if current_char == '{':
+                    if char != "}":
+                        return False
+                if current_char == '[':
+                    if char != "]":
+                        return False
+        if stack :
+            return False
+        return True
+        
+ # https://leetcode.com/problems/valid-parentheses/submissions/
